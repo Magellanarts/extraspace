254ddc112facd4f83af873d624744ee315cc0a12
Greg Lucas <magellanarts@gmail.com>
2021-01-27 03:54:48 -0800

    compress images

--- src/index.js
+++ src/index.js
@@ -13,3 +13,21 @@ var slider = tns({
   autoplayButtonOutput: false,
   navPosition: document.querySelector('.my-slider').getAttribute('data-position') ? document.querySelector('.my-slider').getAttribute('data-position')  : 'top'
 });
+
+// leadership bios
+document.querySelectorAll('.js-leadership-button').forEach(button => {
+  button.addEventListener('click', function(){
+    let person = this.getAttribute('data-person');
+    
+    console.log(document.querySelector(`.js-leadership-photo[data-photo="${person}"]`));
+
+    // remove active
+    document.querySelector('.js-leadership-button.-is-active').classList.remove('-is-active');
+    document.querySelector('.js-leadership-photo.-is-active').classList.remove('-is-active');
+    document.querySelector('.js-leadership-main.-is-active').classList.remove('-is-active')
+    
+    // show new
+    document.querySelector(`.js-leadership-photo[data-photo="${person}"]`).classList.add('-is-active');
+    document.querySelector(`.js-leadership-main[data-bio="${person}"]`).classList.add('-is-active');
+  });
+});
